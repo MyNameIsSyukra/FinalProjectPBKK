39c2e7682d9504d6b58d247fb651a174f4dc16e3
MyNameIsSyukra <sukrawahyu@gmail.com>
2023-11-22 13:55:21 +0700

    feat/dashboard

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -38,33 +38,13 @@
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
-        <!-- address -->
-        <div class="mt-4">
-            <x-input-label for="address" :value="__('Address')" />
 
-            <x-text-input id="address" class="block mt-1 w-full"
-                            type="address"
-                            name="address" />
-
-            <!-- <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" /> -->
-        </div>
-        <!-- Phone Number -->
-        <div class="mt-4">
-            <x-input-label for="phoneNumber" :value="__('Phone Number')" />
-
-            <x-text-input id="phoneNumber" class="block mt-1 w-full"
-                            type="phoneNumber"
-                            name="phoneNumber" />
-
-            <!-- <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" /> -->
-        </div>
-        
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
 
-            <x-primary-button class="ml-4">
+            <x-primary-button class="ms-4">
                 {{ __('Register') }}
             </x-primary-button>
         </div>
