b697fb74735b4986bcfa427a8e9e078cbdfca4f8
Testtsts <ihsannur0610@gmail.com>
2023-11-26 18:10:49 +0700

    [Ihsan] fix registration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('address');
+            $table->string('address')->nullable();
             $table->string('phoneNumber');
             $table->string('role');
             $table->rememberToken();
