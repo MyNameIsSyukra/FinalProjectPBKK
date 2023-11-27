2e896a65c15515f385f03f19826670a6f7bc8e8d
Testtsts <ihsannur0610@gmail.com>
2023-11-27 12:10:04 +0700

    [Ihsan] feat/addToCart function

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProfileController;
 use App\Models\product;
 use App\Http\Controllers\homepageController;
@@ -56,5 +57,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::post('checkout/{productId}',[CartController::class,'addToCart']);
 
 require __DIR__ . '/auth.php';
